505ff1a74644ea742558f16e384ba30554852fc6
aroussetplanat <101940531+aroussetplanat@users.noreply.github.com>
2024-04-07 12:30:32 +0100

    Delete mlag2.m

--- mlag2.m
+++
@@ -1,8 +0,0 @@
-function [ Ylag ] = mlag2 ( Y_raw , p )
-% Mlag2 creates a matrix of p lags from Y_raw
-Y = Y_raw ;
-[T , N ]= size ( Y );
-Ylag = zeros (T , N * p );
-for i =1: p
-Ylag ( p +1: T ,( N *( i -1)+1): N * i )= Y ( p +1 - i :T -i ,1: N );
-end
